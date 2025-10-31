f95bbf242b99cc1ce69405049aef74f623c1eae4
Chemthunder <chemthunderisme@gmail.com>
2025-10-31 10:49:11 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,68 @@
+namespace SpriteKind {
+    export const particle = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (playerSprite.isHittingTile(CollisionDirection.Bottom)) {
+        playerSprite.vy = -140
+    } else {
+        animation.runImageAnimation(
+        playerSprite,
+        [img`
+            9 9 9 9 9 
+            9 9 9 9 9 
+            9 9 9 9 9 
+            9 9 9 9 9 
+            9 9 9 9 9 
+            `,img`
+            1 1 1 1 1 
+            1 1 1 1 1 
+            1 1 1 1 1 
+            1 1 1 1 1 
+            1 1 1 1 1 
+            `],
+        500,
+        false
+        )
+        playerSprite.setVelocity(0, -330)
+        scene.cameraShake(2, 100)
+        for (let index = 0; index < 10; index++) {
+            dashParticle1 = sprites.create(img`
+                . 9 . 
+                9 9 9 
+                . 9 . 
+                `, SpriteKind.particle)
+            dashParticle1.setPosition(playerSprite.x, playerSprite.y)
+            animation.runImageAnimation(
+            dashParticle1,
+            [img`
+                . 9 . 
+                9 9 9 
+                . 9 . 
+                `,img`
+                9 . 9 
+                . 9 . 
+                9 . 9 
+                `],
+            250,
+            true
+            )
+            dashParticle1.setFlag(SpriteFlag.AutoDestroy, true)
+            dashParticle1.setVelocity(0, 20)
+            pause(10)
+        }
+    }
+})
+let dashParticle1: Sprite = null
+let playerSprite: Sprite = null
+playerSprite = sprites.create(img`
+    1 1 1 1 1 
+    1 1 1 1 1 
+    1 1 1 1 1 
+    1 1 1 1 1 
+    1 1 1 1 1 
+    `, SpriteKind.Player)
+controller.moveSprite(playerSprite, 100, 0)
+tiles.setCurrentTilemap(tilemap`level1`)
+scene.cameraFollowSprite(playerSprite)
+tiles.placeOnTile(playerSprite, tiles.getTileLocation(1, 14))
+playerSprite.ay = 500
